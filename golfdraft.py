42226e5cd56a3558b75f4b121a5e023522635d10
root <root@DESKTOP-SFOJQ23.localdomain>
2023-05-19 17:11:02 +0100

    update leaderboard

--- golfdraft.py
+++ golfdraft.py
@@ -87,23 +87,47 @@ for player, selections in players.items():
     sorted_data = sorted(filtered_data, key=lambda x: x["score"])
 
     # Iterate over the sorted data to populate player_data
+    num_rounds = 0
     for player_info in sorted_data:
         player_data["Name"].append(
             player_info["first_name"] + " " + player_info["last_name"])
         player_data["Score"].append(player_info["score"])
 
         rounds = player_info["rounds"]
+        # Update the maximum number of rounds played
+        num_rounds = max(num_rounds, len(rounds))
+
         for i, round_info in enumerate(rounds, start=1):
-            player_data[f"{i} Thru"] = player_data.get(
-                f"{i} Thru", [])
+            player_data[f"{i} Thru"] = player_data.get(f"{i} Thru", [])
+            player_data[f"{i} Score"] = player_data.get(f"{i} Score", [])
 
             player_data[f"{i} Thru"].append(round_info["thru"])
+            if round_info["thru"] == 18:
+                player_data[f"{i} Score"].append(round_info["strokes"])
+            else:
+                # None if the round is not completed yet
+                player_data[f"{i} Score"].append(None)
 
     # Create a DataFrame from the player_data dictionary
     df = pd.DataFrame.from_dict(player_data)
 
+    # Drop rows where any "Thru" column hasn't reached 18
+    df_completed_rounds = df.dropna(
+        subset=[f"{i} Thru" for i in range(1, num_rounds + 1)], how="any")
+
+    # Hide the scores for incomplete rounds
+    for i in range(1, num_rounds + 1):
+        if not all(df_completed_rounds[f"{i} Thru"] == 18):
+            df_completed_rounds[f"{i} Score"] = None
+
+    # Remove the "Thru" column for completed rounds
+    for i in range(1, num_rounds + 1):
+        if all(df_completed_rounds[f"{i} Thru"] == 18):
+            df_completed_rounds = df_completed_rounds.drop(
+                columns=[f"{i} Thru"])
+
     # Add the DataFrame to the player_tables dictionary
-    player_tables[player] = df
+    player_tables[player] = df_completed_rounds
 
 # Concatenate all tables into a single DataFrame
 combined_df = pd.concat(player_tables.values())
@@ -136,3 +160,4 @@ for position, (player, df) in enumerate(sorted_player_tables):
     st.subheader(f"{player} | Total: {overall_score} {emoji}")
     st.write(df)
     st.write("\n")
+
