ec1172b92cba8d5d3f58defe2bf2cf3b4b513a98
garnierfrooc <49648381+garnierfrooc@users.noreply.github.com>
2025-04-04 21:58:03 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o /root/rawboard.json --location 'https://api.sportradar.us/golf/trial/pga/v3/en/2021/tournaments/790dc350-9dd7-4cbd-bd71-ee1e065d323d/leaderboard.json?api_key=LFqiquVQAd6RSldTRygPiacQK8Ub2BOu3WTXYMvZ'
+curl -o /root/rawboard.json --location 'https://api.sportradar.us/golf/trial/pga/v3/en/2025/tournaments/2cba1945-dc1c-4131-92f4-cfdac8c45060/leaderboard.json?api_key=1RGi6YGiXgDcJoXN0kWxx3HMSqJLSKFKZitbDK3Q'
 python3 mungedata.py
 cd /root/golfdraft
 git pull
